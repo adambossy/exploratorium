afd1edadd33fd0827ab085329686b5e88e53105d
Adam <adambossy@gmail.com>
2015-09-27 15:53:16 -0700

    Initial Commit

---
+++ Exploratorium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exploratorium
+//
+//  Created by Adam Bossy on 9/27/15.
+//  Copyright (c) 2015 Bossy Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

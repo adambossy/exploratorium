c0edb5e9496e0a52dd45673e806962179e9cfbe1
Adam <adambossy@gmail.com>
2015-10-19 11:17:14 -0700

    Code clean up.

--- Exploratorium/GraphView.swift
+++ Exploratorium/GraphView.swift
@@ -37,15 +37,12 @@ class GraphView: UIScrollView, UIScrollViewDelegate {
         (100, 167, 11),
         (101, 141, 27)
         ]
-//    var graph : [NodeView: [NodeView]]
+
     var titleDelegate : GraphViewDelegate!
     var newestNodeView : NodeView?
     var containerView : UIView!
 
-//    @IBOutlet weak var containerView: UIView!
-
     required init(coder aDecoder: NSCoder) {
-//        graph = [NodeView: [NodeView]]()
         super.init(coder: aDecoder)
         delegate = self
     }
@@ -67,41 +64,10 @@ class GraphView: UIScrollView, UIScrollViewDelegate {
         containerView.backgroundColor = UIColor.whiteColor()
         self.addSubview(containerView)
 
-        // TODO: Move to IB. Come back to this later.
-//        let titleRect = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: screenSize.width, height: 22))
-//        let titleLabel = UILabel(frame: titleRect)
-//        titleLabel.text = "Exploratorium"
-//        titleLabel.numberOfLines = 0
-//        titleLabel.font = .boldSystemFontOfSize(36.0)
-//        titleLabel.shadowColor = .lightGrayColor()
-//        titleLabel.shadowOffset = CGSize(width: 0, height: 1)
-//        titleLabel.alpha = 0.25
-//        titleLabel.sizeToFit()
-//        titleLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
-//        containerView.addSubview(titleLabel)
-//        containerView.addConstraint(NSLayoutConstraint(
-//            item: titleLabel,
-//            attribute: .CenterX,
-//            relatedBy: .Equal,
-//            toItem: containerView,
-//            attribute: .CenterX,
-//            multiplier: 1,
-//            constant: 0))
-//        containerView.addConstraint(NSLayoutConstraint(
-//            item: titleLabel,
-//            attribute: .CenterY,
-//            relatedBy: .Equal,
-//            toItem: containerView,
-//            attribute: .CenterY,
-//            multiplier: 1,
-//            constant: 0))
-
         // NOTE: Zooming in not interesting, zooming out is. Fix scale to reflect that.
         minimumZoomScale = 0.1
         maximumZoomScale = 5.0
         zoomScale = 1.0
-        
-//        centerContainerView()
     }
 
     override func setNeedsDisplay() {
@@ -111,33 +77,6 @@ class GraphView: UIScrollView, UIScrollViewDelegate {
 
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
         return containerView
-    }
-
-    func scrollViewDidZoom(scrollView: UIScrollView) {
-        println(String(format: "zoomScale: %.5f bounds width: %.5f height %.5f offset x %.5f y %.5f", zoomScale, containerView.frame.size.width, containerView.frame.size.height, self.contentOffset.x, self.contentOffset.y))
-          // NOTE: Hard to do without glitchiness. Figure out later.
-//        if (zoomScale < minimumZoomScale) {
-//            centerContainerView()
-//        }
-    }
-    
-    func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView!, atScale scale: CGFloat) {
-        println(String(format: "zooming ended! %.5f", zoomScale))
-    }
-
-    func centerContainerView() {
-        let boundsSize = bounds.size
-        var contentsFrame = containerView.frame
-        
-        if contentsFrame.size.width < boundsSize.width {
-            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0
-        }
-        
-        if contentsFrame.size.height < boundsSize.height {
-            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0
-        }
-        
-        containerView.frame = contentsFrame
     }
 
     @IBAction func longTap(sender: UILongPressGestureRecognizer) {
@@ -151,7 +90,6 @@ class GraphView: UIScrollView, UIScrollViewDelegate {
                 blue: RGB_VALUES[index].2 / 255.0,
                 alpha: 1.0)
             let nodeView = NodeView(x: location.x - 50, y: location.y - 50, title: nil, color: color)
-//            graph[nodeView] = [NodeView]()
             newestNodeView = nodeView
             containerView.addSubview(nodeView)
             println("Created node! Tiny Rick!")

ec27e0502d5aec1d0f71ee3413aee67954b0350e
Adam <adambossy@gmail.com>
2015-10-22 16:03:16 -0700

    Style NoteCell.

--- NoteCell.swift
+++ NoteCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class NoteCell : UITableViewCell {
 
+    let RGB_VALUES : [(CGFloat, CGFloat, CGFloat)] = [
+        (155, 196, 226),
+    ]
+
     @IBOutlet weak var styleView : UIView!
     @IBOutlet weak var noteLabel : UILabel!
 
@@ -18,6 +22,16 @@ class NoteCell : UITableViewCell {
 
         self.styleView.layer.cornerRadius = 10
         self.styleView.clipsToBounds = true
+
+        let index = Int(arc4random_uniform(UInt32(RGB_VALUES.count)))
+        let color = UIColor(
+            red: RGB_VALUES[index].0 / 255.0,
+            green: RGB_VALUES[index].1  / 255.0,
+            blue: RGB_VALUES[index].2 / 255.0,
+            alpha: 1.0)
+        self.styleView.backgroundColor = color
+        self.styleView.layer.borderColor = UIColor.blackColor().CGColor
+        self.styleView.layer.borderWidth = 4
 
         let views = ["styleView" : styleView]
         let formatString = "|-10-[styleView]-10-|"

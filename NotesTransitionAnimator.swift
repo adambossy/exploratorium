43642714fb049fae0486f028cd1ede9093f13dd6
Adam <adambossy@gmail.com>
2015-10-22 09:54:11 -0700

    Clean up animation code and remove spring effect.

--- NotesTransitionAnimator.swift
+++ NotesTransitionAnimator.swift
@@ -16,9 +16,7 @@ class NotesTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
     // animate a change from one viewcontroller to another
     func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
         
-        // get reference to our fromView, toView and the container view that we should perform the transition in
         let container = transitionContext.containerView()
-        container.backgroundColor = UIColor.clearColor() // Remove black background
 
         let notesView = transitionContext.viewForKey(presenting ? UITransitionContextToViewKey : UITransitionContextFromViewKey)!
         let graphView = transitionContext.viewForKey(presenting ? UITransitionContextFromViewKey : UITransitionContextToViewKey)!
@@ -54,34 +52,27 @@ class NotesTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         container.addSubview(notesView)
         container.bringSubviewToFront(notesView)
 
-        // get the duration of the animation
-        // DON'T just type '0.5s' -- the reason why won't make sense until the next post
-        // but for now it's important to just follow this approach
         let duration = self.transitionDuration(transitionContext)
         
-        // perform the animation!
-        // for this example, just slid both fromView and toView to the left at the same time
-        // meaning fromView is pushed off the screen and toView slides into view
-        // we also use the block animation usingSpringWithDamping for a little bounce
-        UIView.animateWithDuration(duration, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.8, options: nil, animations: {
-            
-            notesView.transform = self.presenting ? CGAffineTransformIdentity : scaleTransform
-            notesView.center = CGPoint(x: CGRectGetMidX(finalFrame),
-                y: CGRectGetMidY(finalFrame))
-            notesView.alpha = self.presenting ? 1.0 : 0.0
-
-            
+        UIView.animateWithDuration(
+            duration,
+            delay: 0,
+            options: nil,
+            animations: {
+                notesView.transform = self.presenting ? CGAffineTransformIdentity : scaleTransform
+                notesView.center = CGPoint(x: CGRectGetMidX(finalFrame),
+                    y: CGRectGetMidY(finalFrame))
+                notesView.alpha = self.presenting ? 1.0 : 0.0
             }, completion: { finished in
-                
                 // tell our transitionContext object that we've finished animating
                 transitionContext.completeTransition(true)
-                
-        })
+            }
+        )
         
     }
 
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
-        return 1
+        return 0.4
     }
     
 }
